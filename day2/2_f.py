5be845b2bc1364727f748d1e8a2dbc0eb18e231e
Maretti Filippo <69721649+WeridFire@users.noreply.github.com>
2022-12-03 09:44:07 +0100

    mysol

--- day2/2_f.py
+++ day2/2_f.py
@@ -11,7 +11,51 @@ value_paper = 2
 value_scissor = 3
 win = 6
 
+WIN = "Z"
+LOSE = "X"
+DRAW = "Y"
 
+def calcola_punteggio(strs:str):
+
+    if strs == rock:
+        return value_rock
+    elif strs == paper:
+        return value_paper
+    else:
+        return value_scissor
+
+def second_part(lines:list):
+
+    tot = 0
+
+
+    for line in lines:
+        #caso in cui bisogna pareggiare
+        if line[2] == DRAW:
+            tot = tot + calcola_punteggio(line[0]) + 3
+        
+        elif line[2] == WIN:
+            tot += 6
+            if line[0] == rock:
+                tot += value_paper
+            
+            elif line[0] == paper:
+                tot += value_scissor
+            
+            elif line[0] == scissor:
+                tot += value_rock
+        
+        else:
+            if line[0] == rock:
+                tot += value_scissor
+            
+            elif line[0] == paper:
+                tot += value_rock
+            
+            elif line[0] == scissor:
+                tot += value_paper
+                
+    return tot
 
 def who_won(lines:list):
 
@@ -75,6 +119,9 @@ lines = file.readlines()
 
 total = who_won(lines)
 
+tot = second_part(lines)
+
 print(total)
+print(tot)
         
 
