b35d7d29151630a199198e2f968e7e140cf5c27d
Giacomo Maggiore <giaco.maggiore@gmail.com>
2022-12-07 23:04:39 +0100

    h

--- day7/7_g.py
+++ day7/7_g.py
@@ -16,29 +16,50 @@ dir = Tree("root")
 current = dir
 
 def aggiungi(Parent, nome):
-    temp = Tree()
+    temp = Tree(nome)
     temp.name = nome
 
     Parent.children.append(temp)
 
 def file(Current, file):
     Current.data = Current.data + file
-
+    print(Current.data)
 
 def cd(Current, nome):
-    temp = Current.children[Current.children.index(nome)]
+    for sottocartelle in Current.children:
+        if sottocartelle.name == nome:
+            temp = sottocartelle
+
+def calcola(Directory):
+    dimensione = 0
+    if len(Directory.children) == 0:
+        print("vuoto")
+        dimensione = Directory.data
+        #print(dimensione)
+    else:
+        print("Non vuoto")
+        for sottocartelle in  Directory.children:
+        
+            dimensione = dimensione + calcola(sottocartelle)
+        
+    
+    return dimensione
+    
 
 for line in lista_comandi:
     if "dir" in line:
         nome = line[5:]
-        aggiungi(nome)
+        aggiungi(current,nome)
 
     elif "cd" in line:
-        nome = line[3:]
+        nome = line[5:]
+        aggiungi(current, nome)
         cd(current, nome)
 
     elif "ls" not in line and "dir" not in line:
         nome = line.split(" ", 1)[0]
         numero = int(nome)
+        #print(nome)
         file(current, numero)
    
+print(calcola(dir))
