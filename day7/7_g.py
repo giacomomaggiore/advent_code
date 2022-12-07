640b72e603dd9c069891c60448dc487c9b11cd06
Giacomo Maggiore <giaco.maggiore@gmail.com>
2022-12-07 18:25:39 +0100

    jh

--- day7/7_g.py
+++ day7/7_g.py
@@ -1,8 +1,33 @@
 file = open("day7\input_g.txt")
 lista_comandi:list = file.readlines()
+directory = []
+
+
+class Tree:
+    def __init__(self, data):
+        self.children = []
+        self.data = data
+
+directory = Tree()
+current = Tree()
+
+
+def aggiungi_directory(Parent, Children):
+    Parent.children.append(Children)
+
+def cd(Current):
+    current = Current
+
+def inserisci_file(Parent, file):
+    Parent.data = file
+
 for line in lista_comandi:
-    if line[0:3] == "$ ls":
+    if "$ ls" in line:
         lista_comandi.remove(line)
 
+for line in lista_comandi:
     
+        
+
+
 print(lista_comandi)
