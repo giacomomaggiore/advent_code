89ab1612681da702988e5c1eb8a7fb6cf8177329
Giacomo Maggiore <giaco.maggiore@gmail.com>
2022-12-07 19:33:36 +0100

    h

--- day7/7_g.py
+++ day7/7_g.py
@@ -1,33 +1,38 @@
 file = open("day7\input_g.txt")
 lista_comandi:list = file.readlines()
-directory = []
+lista_comandi_reverse = lista_comandi.reverse()
+
 
 
 class Tree:
-    def __init__(self, data):
+    def __init__(self, nome):
+        self.name = nome
         self.children = []
-        self.data = data
+        self.data = 0 
 
-directory = Tree()
-current = Tree()
+dir = Tree("root")
+current = dir
+
+def aggiungi(Parent, nome):
+    temp = Tree()
+    temp.name = nome
+
+    Parent.children.append(temp)
+
+def file(Current, file):
+    Current.data = Current.data + file
 
 
-def aggiungi_directory(Parent, Children):
-    Parent.children.append(Children)
-
-def cd(Current):
-    current = Current
-
-def inserisci_file(Parent, file):
-    Parent.data = file
+def cd(Current, nome):
+    temp = Current.children[Current.children.index(nome)]
 
 for line in lista_comandi:
-    if "$ ls" in line:
-        lista_comandi.remove(line)
+    if "dir" in line:
+        pass
+    elif "cd" in line:
+        pass
+    elif "ls" in line:
+        pass
+    else:
+        pass
 
-for line in lista_comandi:
-    
-        
-
-
-print(lista_comandi)
